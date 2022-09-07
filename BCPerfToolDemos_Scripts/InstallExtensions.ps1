f2856819d976306b66f54cec43a95c40675b429c
waldo <wauteri@ifacto.be>
2022-09-07 11:43:36 +0200

    Updated scripts to run on multiple containers

--- BCPerfToolDemos_Scripts/InstallExtensions.ps1
+++ BCPerfToolDemos_Scripts/InstallExtensions.ps1
@@ -1,8 +1,10 @@
+$containername = 'bccurrent'
+
 $UserName = 'waldo'
 $Password = ConvertTo-SecureString 'Waldo1234' -AsPlainText -Force
 $ContainerCredential = New-Object System.Management.Automation.PSCredential ($UserName, $Password)
 
-# Invoke-ScriptInBcContainer -containerName bccurrent -scriptblock {
+# Invoke-ScriptInBcContainer -containerName $containername -scriptblock {
 #     function PublishApp($AppName){
     
 #     Get-NAVAppInfo -ServerInstance BC | where name -like $AppName | Uninstall-NAVApp -DoNotSaveData -Force 
@@ -13,11 +15,11 @@ $ContainerCredential = New-Object System.Management.Automation.PSCredential ($Us
 # } 
 
 function PublishExtensionApp($AppName){
-    $AppExists = Get-BcContainerAppInfo -containerName bccurrent | where Name -eq $AppName
+    $AppExists = Get-BcContainerAppInfo -containerName $containername | where Name -eq $AppName
     if ($AppExists){
-        Install-BcContainerApp -containerName bccurrent -appName $AppName        
+        Install-BcContainerApp -containerName $containername -appName $AppName        
     } else {
-        Publish-BcContainerApp -containerName bccurrent -credential $ContainerCredential -ignoreIfAppExists -appFile "C:\_Source\Community\waldo.BCPerfTool\$($AppName)\waldo_$($AppName)_1.0.0.0.app" -skipVerification -sync ForceSync -useDevEndpoint -install
+        Publish-BcContainerApp -containerName $containername -credential $ContainerCredential -ignoreIfAppExists -appFile "C:\_Source\Community\waldo.BCPerfTool\$($AppName)\waldo_$($AppName)_1.0.0.0.app" -skipVerification -sync ForceSync -useDevEndpoint -install
     }
 }
 
--- BCPerfToolDemos_Scripts/UnInstallAllExtensionApps.ps1
+++ BCPerfToolDemos_Scripts/UnInstallAllExtensionApps.ps1
@@ -1,4 +1,6 @@
-Invoke-ScriptInBcContainer -containerName bccurrent -scriptblock {
+$containername = 'bccurrent'
+
+Invoke-ScriptInBcContainer -containerName $containername -scriptblock {
     Get-NAVAppInfo -ServerInstance bc -Tenant default -TenantSpecificProperties | 
         where name -like 'BCPerfToolDemos_Ext*' | % {
             $_ | UnInstall-NavApp -Verbose
