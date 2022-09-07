d0c0861ae897f1b863c93165efcc1c26533720cd
waldo <wauteri@ifacto.be>
2022-09-07 11:48:25 +0200

    Renamed scripts

--- BCPerfToolDemos_Scripts/PublishAndInstallExtensions.ps1
+++ BCPerfToolDemos_Scripts/PublishAndInstallExtensions.ps1
@@ -4,16 +4,6 @@ $UserName = 'waldo'
 $Password = ConvertTo-SecureString 'Waldo1234' -AsPlainText -Force
 $ContainerCredential = New-Object System.Management.Automation.PSCredential ($UserName, $Password)
 
-# Invoke-ScriptInBcContainer -containerName $containername -scriptblock {
-#     function PublishApp($AppName){
-    
-#     Get-NAVAppInfo -ServerInstance BC | where name -like $AppName | Uninstall-NAVApp -DoNotSaveData -Force 
-#     Get-NAVAppInfo -ServerInstance BC | where name -like $AppName | UnPublish-NavApp     
-#     }
-    
-#     PublishApp -Verbose -AppName "BCPerfToolDemos_Ext*"
-# } 
-
 function PublishExtensionApp($AppName){
     $AppExists = Get-BcContainerAppInfo -containerName $containername | where Name -eq $AppName
     if ($AppExists){
---
+++ BCPerfToolDemos_Scripts/PublishAndInstallV21Extensions.ps1
@@ -0,0 +1,18 @@
+$containername = 'bcdaily'
+
+$UserName = 'waldo'
+$Password = ConvertTo-SecureString 'Waldo1234' -AsPlainText -Force
+$ContainerCredential = New-Object System.Management.Automation.PSCredential ($UserName, $Password)
+
+function PublishExtensionApp($AppName){
+    $AppExists = Get-BcContainerAppInfo -containerName $Containername | where Name -eq $AppName
+    if ($AppExists){
+        Install-BcContainerApp -containerName $Containername -appName $AppName        
+    } else {
+        Publish-BcContainerApp -containerName $Containername -credential $ContainerCredential -ignoreIfAppExists -appFile "C:\_Source\Community\waldo.BCPerfTool\$($AppName)\waldo_$($AppName)_1.0.0.0.app" -skipVerification -sync ForceSync -useDevEndpoint -install
+    }
+}
+
+PublishExtensionApp -AppName 'BCPerfTool'
+PublishExtensionApp -AppName 'BCPerfToolDemos'
+PublishExtensionApp -AppName 'BCPerfToolDemos - V21'
